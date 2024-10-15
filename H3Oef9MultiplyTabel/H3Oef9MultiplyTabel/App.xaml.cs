82cb0751244b9848073d02103e827dc93c042346
Indy Iri <indy.iri@student.pxl.be>
2024-10-15 21:43:16 +0200

    WPF interface gemaakt

---
+++ H3Oef9MultiplyTabel/H3Oef9MultiplyTabel/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace H3Oef9MultiplyTabel
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ H3Oef9MultiplyTabel/H3Oef9MultiplyTabel/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace H3Oef9MultiplyTabel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultTextBox.Clear();
+        }
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
